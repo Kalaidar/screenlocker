f033cbf859490b04014abb59885965c976a7303f
Kalaidar <net.pyat@yandex.ru>
2017-12-22 01:30:13 +0300

    commit (=

--- Space-Invaders/main.py
+++ Space-Invaders/main.py
@@ -1,5 +1,43 @@
 import tkinter as tk
+from ctypes import windll, byref, create_unicode_buffer, create_string_buffer
+FR_PRIVATE  = 0x10
+FR_NOT_ENUM = 0x20
 
+def loadfont(fontpath, private=True, enumerable=False):
+    '''
+    Makes fonts located in file `fontpath` available to the font system.
+
+    `private`     if True, other processes cannot see this font, and this 
+                  font will be unloaded when the process dies
+    `enumerable`  if True, this font will appear when enumerating fonts
+
+    See https://msdn.microsoft.com/en-us/library/dd183327(VS.85).aspx
+
+    '''
+    # This function was taken from
+    # https://github.com/ifwe/digsby/blob/f5fe00244744aa131e07f09348d10563f3d8fa99/digsby/src/gui/native/win/winfonts.py#L15
+    # This function is written for Python 2.x. For 3.x, you
+    # have to convert the isinstance checks to bytes and str
+    if isinstance(fontpath, bytes):
+        pathbuf = create_string_buffer(fontpath)
+        AddFontResourceEx = windll.gdi32.AddFontResourceExA
+    elif isinstance(fontpath, bytes):
+        pathbuf = create_unicode_buffer(str)
+        AddFontResourceEx = windll.gdi32.AddFontResourceExW
+    else:
+        raise TypeError('fontpath must be of type str or bytes')
+
+    flags = (FR_PRIVATE if private else 0) | (FR_NOT_ENUM if not enumerable else 0)
+    numFontsAdded = AddFontResourceEx(byref(pathbuf), flags, 0)
+    return bool(numFontsAdded)
+
+class mainWindow(tk.Frame):
+    def __init__(self, parent):
+        tk.Frame.__init__(self, parent)
+        self.header = tk.Label(self, text="SPACE INVADERS", anchor="w", font=("invasion2000",80))
+        self.header.pack(side="top")
+        
+        
 class Example(tk.Frame):
     def __init__(self, parent):
         tk.Frame.__init__(self, parent)
@@ -35,8 +73,10 @@ class Example(tk.Frame):
 
 if __name__ == "__main__":
     root = tk.Tk()
-    root.title('Eve Offline')
+    root.title('SPACE INVADERS')
     root.resizable(width=False, height=False)
-    root.geometry('{}x{}'.format(1000, 500))
-    Example(root).pack(fill="both", expand=True)
+    root.geometry('{}x{}'.format(1200, 600))
+    root.tk_setPalette(background="black", fg="lavender")
+    loadfont(bytes('gfx\invasion2000.ttf', encoding='utf-8'))
+    mainWindow(root).pack(fill="both", expand=True)
     root.mainloop()
