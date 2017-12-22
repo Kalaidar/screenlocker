43f67fbb01699af1878aea8e2f4f731cc8b51f3d
Kalaidar <net.pyat@yandex.ru>
2017-12-22 10:43:03 +0300

    mod

--- Space-Invaders/main.py
+++ Space-Invaders/main.py
@@ -1,35 +1,6 @@
 import tkinter as tk
-from ctypes import windll, byref, create_unicode_buffer, create_string_buffer
-FR_PRIVATE  = 0x10
-FR_NOT_ENUM = 0x20
+import spaceLib
 
-def loadfont(fontpath, private=True, enumerable=False):
-    '''
-    Makes fonts located in file `fontpath` available to the font system.
-
-    `private`     if True, other processes cannot see this font, and this 
-                  font will be unloaded when the process dies
-    `enumerable`  if True, this font will appear when enumerating fonts
-
-    See https://msdn.microsoft.com/en-us/library/dd183327(VS.85).aspx
-
-    '''
-    # This function was taken from
-    # https://github.com/ifwe/digsby/blob/f5fe00244744aa131e07f09348d10563f3d8fa99/digsby/src/gui/native/win/winfonts.py#L15
-    # This function is written for Python 2.x. For 3.x, you
-    # have to convert the isinstance checks to bytes and str
-    if isinstance(fontpath, bytes):
-        pathbuf = create_string_buffer(fontpath)
-        AddFontResourceEx = windll.gdi32.AddFontResourceExA
-    elif isinstance(fontpath, bytes):
-        pathbuf = create_unicode_buffer(str)
-        AddFontResourceEx = windll.gdi32.AddFontResourceExW
-    else:
-        raise TypeError('fontpath must be of type str or bytes')
-
-    flags = (FR_PRIVATE if private else 0) | (FR_NOT_ENUM if not enumerable else 0)
-    numFontsAdded = AddFontResourceEx(byref(pathbuf), flags, 0)
-    return bool(numFontsAdded)
 
 class mainWindow(tk.Frame):
     def __init__(self, parent):
@@ -55,28 +26,15 @@ class Example(tk.Frame):
         self.output.pack(side="top", fill="x", expand=True)
         self.submit.pack(side="right")
 
-    def calculate(self):
-        # get the value from the input widget, convert
-        # it to an int, and do a calculation
-        try:
-            i = int(self.entry.get())
-            result = "%s*2=%s" % (i, i*2)
-        except ValueError:
-            result = "Please enter digits only"
-
-        # set the output widget to have our result
-        self.output.configure(text=result)
-
-# if this is run as a program (versus being imported),
-# create a root window and an instance of our example,
-# then start the event loop
 
 if __name__ == "__main__":
+    spaceLib.loadfont(bytes('gfx\invasion2000.ttf', encoding='utf-8'))
+    tkRootWidth = 1200
+    tkRootHeight = 600
     root = tk.Tk()
     root.title('SPACE INVADERS')
     root.resizable(width=False, height=False)
-    root.geometry('{}x{}'.format(1200, 600))
+    root.geometry('{}x{}'.format(tkRootWidth, tkRootHeight))
     root.tk_setPalette(background="black", fg="lavender")
-    loadfont(bytes('gfx\invasion2000.ttf', encoding='utf-8'))
     mainWindow(root).pack(fill="both", expand=True)
     root.mainloop()
