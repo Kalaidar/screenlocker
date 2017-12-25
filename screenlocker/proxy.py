f9727c62debcfbad10f0355e83b980daf05fffc0
Kalaidar <net.pyat@yandex.ru>
2017-12-25 10:49:23 +0300

    proxy added

---
+++ screenlocker/proxy.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import socket
+import thread
+import time
+import datetime
+
+def socketServer():
+    sockServer = socket.socket()
+    sockServer.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sockServer.bind(('', 49781))
+    sockServer.listen(1)
+    return sockServer
+
+sockClient = socket.socket()
+sockClient.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+sockClient.bind(('', 49782))
+sockClient.listen(1)
+
+def logWrite(message):
+    print(message)
+    ts = time.time()
+    ts = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+    with open('/var/log/proxy.log', 'a') as f:
+        f.write(ts + " " + message + '\n')
+
+
+def serverListener(sockServer):
+    global connServer
+    while True:
+        try:
+            connServer, addrClient = sockServer.accept()
+        except:
+            continue
+        logWrite('server connected\n')
+
+sockServer = socketServer()
+thread.start_new_thread(serverListener, (sockServer,))
+
+while True:
+    connClient, addrClient = sockClient.accept()
+    if (connClient):
+        logWrite('client connected')
+        try:
+            connServer.send('b')
+            connClient.shutdown(socket.SHUT_RDWR)
+        except:
+#           sockServer.shutdown(socket.SHUT_RDWR)
+            connClient.shutdown(socket.SHUT_RDWR)
+#           time.sleep(5)
+#           sockServer = socketServer()
